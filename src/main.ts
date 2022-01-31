031a3caf90791f6daf1a73cf586d4538af7ba3ac
Srihari Vishnu <srihari.vishnu@gmail.com>
2022-01-31 03:54:47 -0500

    test: git action

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {DirectedGraph, IGraph, Node} from './graph'
 import {ICruiseResult, IModule, cruise} from 'dependency-cruiser'
 
-const ARRAY_OF_FILES_AND_DIRS_TO_CRUISE = ['.']
+const ARRAY_OF_FILES_AND_DIRS_TO_CRUISE = ['src/']
 const cruiseOptions = {
   includeOnly: '^src'
 }
@@ -36,6 +37,23 @@ async function run(): Promise<void> {
     for (const module of cruiseResult.modules) {
       buildGraphFromModule(graph, module)
     }
+
+    const github_token = core.getInput('GITHUB_TOKEN')
+
+    const context = github.context
+    if (context.payload.pull_request == null) {
+      core.setFailed('No pull request found.')
+      return
+    }
+    const pull_request_number = context.payload.pull_request.number
+
+    const octokit = github.getOctokit(github_token)
+
+    await octokit.rest.issues.createComment({
+      ...context.repo,
+      issue_number: pull_request_number,
+      body: ` \`\`\`${graph.toString()}\`\`\``
+    })
 
     core.debug(graph.toString())
 
